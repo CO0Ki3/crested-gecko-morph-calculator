74226df830684679687dc91b52892a7e0ee2715e
co0ki3 <ehrrhgus0324@gmail.com>
2022-08-13 20:28:12 +0900

    feature: Button

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@mantine/core';
+
+function CalcButton() {
+  return (
+    <Button radius='md' sx={{ width: '100%' }}>
+      계산하기
+    </Button>
+  );
+}
+
+export default CalcButton;
