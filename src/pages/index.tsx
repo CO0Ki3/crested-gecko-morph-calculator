36d07c8b7c59d8a3c89e06b402feee6d621e854a
co0ki3 <ehrrhgus0324@gmail.com>
2022-08-13 20:28:27 +0900

    feature: border, styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,27 @@
-import { Box, Container } from '@mantine/core';
+import { Box, Container, Group } from '@mantine/core';
+import CalcButton from '../components/Button';
 import DropdownList from '../components/dropdownList';
 
 function Main() {
   return (
-    <Container sx={{ width: '50%' }}>
+    <Container
+      sx={{
+        width: '65%',
+        boxShadow: '0 0 2px 1px rgb(0 0 0 / 10%)',
+        padding: '30px',
+        borderRadius: '8px',
+      }}
+    >
+      <Group position='apart' grow sx={{ marginBottom: '20px' }}>
+        <Box sx={{ width: '100%' }}>
+          <DropdownList title='수컷' />
+        </Box>
+        <Box sx={{ width: '100%' }}>
+          <DropdownList title='암컷' />
+        </Box>
+      </Group>
       <Box sx={{ width: '100%' }}>
-        <DropdownList title='수컷' />
-        <DropdownList title='암컷' />
+        <CalcButton />
       </Box>
     </Container>
   );
