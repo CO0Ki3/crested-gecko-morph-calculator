abd0aa7dfb227a82a07ca7cfb7c93096f5c17407
co0ki3 <ehrrhgus0324@gmail.com>
2022-08-14 15:49:20 +0900

    feature: add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import { AppShell, Header } from '@mantine/core';
+import HeaderContents from './components/header';
 import Main from './pages';
 
 function App() {
   return (
-    <div>
+    <AppShell
+      header={
+        <Header height={60} sx={{ borderBottom: 'none' }}>
+          <HeaderContents />
+        </Header>
+      }
+    >
       <Main />
-    </div>
+    </AppShell>
   );
 }
 
---
+++ src/components/header/index.tsx
@@ -0,0 +1,17 @@
+function HeaderContents() {
+  return (
+    <div
+      style={{
+        width: '100%',
+        backgroundColor: 'rgba(244, 244, 244, 0.72)',
+        height: '100%',
+        display: 'flex',
+        alignItems: 'center',
+      }}
+    >
+      크레스티드 게코 모프 계산기
+    </div>
+  );
+}
+
+export default HeaderContents;
