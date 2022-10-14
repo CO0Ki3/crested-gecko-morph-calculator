4d13d2b25a97d4e2c2c4592fc575cb803fa3912e
co0ki3 <ehrrhgus0324@gmail.com>
2022-10-14 21:06:09 +0900

    feature: view more

--- src/components/Result/items/children/ChildrenItem.tsx
+++ src/components/Result/items/children/ChildrenItem.tsx
@@ -1,27 +1,46 @@
-import { Text } from '@mantine/core';
+import { Text, Transition } from '@mantine/core';
+import { useLayoutEffect, useState } from 'react';
 
 function ChildrenItem({
   result,
+  isViewMore,
 }: {
   result: {
     gene: string[];
     value: string;
   }[];
+  isViewMore: boolean;
 }) {
+  const [isMount, setIsMount] = useState(false);
+
+  useLayoutEffect(() => {
+    setIsMount(true);
+  }, [isViewMore]);
+
   return result.length === 0 ? (
     <Text sx={{ padding: '30px 0' }}>노말 100%</Text>
   ) : (
     <>
       {result.map((value, i) => (
-        <Text
-          sx={i === 0 ? { padding: '30px 0' } : { paddingBottom: '30px' }}
-          key={`${value.value}-${i + 1}`}
+        <Transition
+          key={value.gene.join('')}
+          transition='fade'
+          mounted={isMount}
+          duration={1000}
         >
-          {value.gene.length === 0
-            ? '노말 '
-            : value.gene.map((gene) => `${gene} `)}
-          {value.value}
-        </Text>
+          {(style) => (
+            <Text
+              sx={i === 0 ? { padding: '30px 0' } : { paddingBottom: '30px' }}
+              style={style}
+              key={`${value.value}-${i + 1}`}
+            >
+              {value.gene.length === 0
+                ? '노말 '
+                : value.gene.map((gene) => `${gene} `)}
+              {value.value}
+            </Text>
+          )}
+        </Transition>
       ))}
     </>
   );
