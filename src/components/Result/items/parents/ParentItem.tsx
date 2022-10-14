21fb02dceb6e25db6f4c486b5cb9c47ab4f2eeb1
co0ki3 <ehrrhgus0324@gmail.com>
2022-10-14 22:52:16 +0900

    feature: morphs decide badge color

--- src/components/Result/items/parents/ParentItem.tsx
+++ src/components/Result/items/parents/ParentItem.tsx
@@ -1,4 +1,104 @@
-import { Badge, Card, Box } from '@mantine/core';
+import { Badge, Card, Box, MantineGradient } from '@mantine/core';
+
+// MantineGradient
+
+const BADGE_BACKGROUND_COLORS: { [key: string]: MantineGradient } = {
+  '아잔틱': {
+    from: '#4B3B33',
+    to: '#B9A290',
+    deg: 60,
+  },
+  '패턴리스': {
+    from: '#D7D2DC',
+    to: '#F8E0C4',
+    deg: 60,
+  },
+  '팬텀': {
+    from: '#C1AFAB',
+    to: '#51362E',
+    deg: 60,
+  },
+  '슈퍼 스트라이프': {
+    from: '#1B1B23',
+    to: '#F0DCC5',
+    deg: 60,
+  },
+  '블루 아이': {
+    from: 'blue',
+    to: '#A77C2D',
+    deg: 60,
+  },
+  '헷 아잔틱': {
+    from: '#4B3B33',
+    to: '#B9A290',
+    deg: 60,
+  },
+  '헷 패턴리스': {
+    from: '#D7D2DC',
+    to: '#F8E0C4',
+    deg: 60,
+  },
+  '햇 팬텀': {
+    from: '#C1AFAB',
+    to: '#51362E',
+    deg: 60,
+  },
+  '햇 블루 아이': {
+    from: 'blue',
+    to: '#A77C2D',
+    deg: 60,
+  },
+  '헷 슈퍼 스트라이프': {
+    from: '#1B1B23',
+    to: '#F0DCC5',
+    deg: 60,
+  },
+  '릴리 화이트': {
+    from: '#D0CFD0',
+    to: '#DED8BA',
+    deg: 60,
+  },
+  '카푸치노': {
+    from: '#563D36',
+    to: '#FEBE75',
+    deg: 60,
+  },
+  '소프트 스케일': {
+    from: '#474642',
+    to: '#B9A798',
+    deg: 60,
+  },
+  '세이블': {
+    from: '#150F17',
+    to: '#EEF7EE',
+    deg: 60,
+  },
+  '슈퍼 릴리 화이트': {
+    from: '#C19897',
+    to: '#C7AD96',
+    deg: 60,
+  },
+  '슈퍼 카푸치노': {
+    from: '#302A2A',
+    to: '#443B38',
+    deg: 60,
+  },
+  '슈퍼 소프트 스케일': {
+    from: '#474642',
+    to: '#B9A798',
+    deg: 60,
+  },
+  '슈퍼 세이블': {
+    from: '#1C171A',
+    to: '#CACFD7',
+    deg: 60,
+  },
+  '노말': {
+    from: 'teal',
+    to: 'blue',
+    deg: 60,
+  },
+};
 
 function ParentItem({ genes }: { genes: string[] }) {
   return (
@@ -27,7 +127,7 @@ function ParentItem({ genes }: { genes: string[] }) {
         : genes.map((value) => (
             <Badge
               variant='gradient'
-              gradient={{ from: 'teal', to: 'blue', deg: 60 }}
+              gradient={BADGE_BACKGROUND_COLORS[value]}
               sx={{
                 marginTop: '10px',
                 marginBottom: '-10px',
