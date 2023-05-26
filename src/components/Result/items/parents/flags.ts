bf7872ebd3fc54ba7996e083a934c515ca0de978
co0ki3 <ehrrhgus0324@gmail.com>
2023-05-26 10:49:01 +0900

    feature: add 릴리화이트

--- src/components/Result/items/parents/flags.ts
+++ src/components/Result/items/parents/flags.ts
@@ -1,7 +1,9 @@
 import 릴리화이트세이블 from '../../../../static/릴리화이트세이블.jpg';
+import 릴리화이트 from '../../../../static/릴리화이트.jpg';
 
 const flags = {
   릴리화이트세이블,
+  릴리화이트,
 };
 
 export default flags as any;
