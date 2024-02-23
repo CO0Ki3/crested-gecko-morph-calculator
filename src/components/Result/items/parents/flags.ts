250ea4ba6e6cd24e729233024cec16a261f0630e
독고현 <dkh0324@midasin.com>
2024-02-23 21:11:26 +0900

    chore: package update

--- src/components/Result/items/parents/flags.ts
+++ src/components/Result/items/parents/flags.ts
@@ -1,9 +1,9 @@
 import 릴리화이트세이블 from '../../../../static/릴리화이트세이블.jpg';
 import 릴리화이트 from '../../../../static/릴리화이트.jpg';
 
-const flags = {
+const flags: { [key: string]: string } = {
   릴리화이트세이블,
   릴리화이트,
 };
 
-export default flags as any;
+export default flags;
