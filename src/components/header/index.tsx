a15cf19087aa8ea9f3438bf4dc62e16d1348ab26
독고현 <dkh0324@midasin.com>
2024-03-14 12:59:17 +0900

    feature: 약간의 반응형 추가

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -8,24 +8,16 @@ function HeaderContents() {
   };
 
   return (
-    <div
-      style={{
-        width: '100%',
-        border: '1px solid rgb(230, 230, 230)',
-        height: '100%',
-        display: 'flex',
-        alignItems: 'center',
-        paddingLeft: '30px',
-        justifyContent: 'space-between',
-        paddingRight: '30px',
-      }}
-    >
+    <div className='header'>
       {t('header')}
-      <Select
-        data={['kr', 'en']}
-        defaultValue='kr'
-        onSearchChange={onChangeLanguage}
-      />
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <div style={{ marginRight: '10px', marginTop: '3px' }}>Language</div>
+        <Select
+          data={['kr', 'en']}
+          defaultValue='kr'
+          onSearchChange={onChangeLanguage}
+        />
+      </div>
     </div>
   );
 }
