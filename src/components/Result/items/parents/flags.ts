27fc0cf5aff73b6f91695162b3ec25f582d6b939
독고현 <dkh0324@midasin.com>
2024-03-15 17:38:02 +0900

    feature: add image

--- src/components/Result/items/parents/flags.ts
+++ src/components/Result/items/parents/flags.ts
@@ -2,12 +2,18 @@ import 릴리화이트세이블 from '../../../../static/릴리화이트세이�
 import 릴리화이트 from '../../../../static/릴리화이트.jpg';
 import 릴리화이트초초 from '../../../../static/릴리화이트초초.jpeg';
 import 초초 from '../../../../static/초초.jpeg';
+import 릴리화이트하이포 from '../../../../static/릴리화이트하이포.jpeg';
+import 슈퍼하이포 from '../../../../static/슈퍼하이포.jpeg';
+import 릴리화이트카푸치노하이포 from '../../../../static/릴리화이트카푸치노하이포.jpeg';
 
 const flags: { [key: string]: string } = {
   릴리화이트세이블,
   릴리화이트,
   릴리화이트초초,
   초초,
+  릴리화이트하이포,
+  슈퍼하이포,
+  릴리화이트카푸치노하이포,
 };
 
 export default flags;
