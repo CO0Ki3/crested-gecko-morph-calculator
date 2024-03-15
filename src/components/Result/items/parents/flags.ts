2b6aedb02090d5987f7331a38daa0a3937b68eec
독고현 <dkh0324@midasin.com>
2024-03-15 16:34:30 +0900

    fix: 이미지 링크 수정

--- src/components/Result/items/parents/flags.ts
+++ src/components/Result/items/parents/flags.ts
@@ -1,7 +1,7 @@
 import 릴리화이트세이블 from '../../../../static/릴리화이트세이블.jpg';
 import 릴리화이트 from '../../../../static/릴리화이트.jpg';
 import 릴리화이트초초 from '../../../../static/릴리화이트초초.jpeg';
-import 초초 from '../../../../static/초초.jpeg';
+import 초초 from '../../../../static/초초.jpeg';
 
 const flags: { [key: string]: string } = {
   릴리화이트세이블,
